dbd77ebbd4131ecfa4e3dd8f0a9ce714f3e0b22d
bradonf333 <bradonf123@gmail.com>
2017-08-15 17:42:15 -0600

    Removing files

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,30 +1,10 @@
-/** Root component for the application */
-
-// Import the Component and Product Service
 import { Component } from '@angular/core';
 
-// Decorator
 @Component({
-    selector: 'pm-app',
-    template: `
-    <div>
-        <nav class='navbar navbar-default'>
-            <div class='container-fluid'>
-                <a class='navbar-brand'>{{pageTitle}}</a>
-                <ul class='nav navbar-nav'>
-                    <li><a [routerLink]="['/welcome']">Home</a></li>
-                    <li><a [routerLink]="['/products']">Product List</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class ='container'>
-            <router-outlet></router-outlet>
-        </div>
-    </div>
-    `
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-// Export class AppComponent
 export class AppComponent {
-    pageTitle: string = 'Acme Product Management';
+  title = 'Angular: Getting Started';
 }
