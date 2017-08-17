c418554237802f39762bf12c6abdb9ab5643d92b
bradonf333 <bradonf123@gmail.com>
2017-08-16 18:24:01 -0600

    Updating template and comments

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,3 +1,5 @@
+/** Root component for the application */
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
