81bc7302d7b90c942201bacc3899bc0878878f27
bradonf333 <bradonf123@gmail.com>
2017-08-17 21:11:30 -0600

    Created a service to retrieve products from a json file

---
+++ APM/src/app/products/product.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { IProduct } from './product';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+
+
+@Injectable()
+export class ProductService {
+
+    // Represents a web api url that would have access to the list of products
+    private _productUrl = 'api/products/products.json';
+
+    /**
+     * Constructor to inject the Http Service from Angular
+     */
+    constructor(private _http: Http) {
+    }
+
+    /**
+     * Returns a list of IProducts by sending an http request to the
+     * given url and casting the json response into an IProduct[]
+     */
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get(this._productUrl)
+        .map((response: Response) => <IProduct[]> response.json())
+        .catch(this.handleError);
+    }
+
+    /**
+     * Takes an error, logs it to the console and throws it to the calling code.
+     * @param error
+     */
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+}
