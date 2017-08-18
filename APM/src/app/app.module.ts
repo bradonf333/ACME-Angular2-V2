ecb20dcf5aa63877a8151f0acb7237c137b2a34b
bradonf333 <bradonf123@gmail.com>
2017-08-17 22:01:08 -0600

    Implemented routes and base for the product-detail page

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, StarComponent
+    AppComponent, ProductListComponent, StarComponent, ProductDetailComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpModule
+    , RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: '', redirectTo: 'products', pathMatch: 'full' },
+      { path: '**', redirectTo: 'products', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
