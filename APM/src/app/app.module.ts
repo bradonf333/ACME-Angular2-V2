7e53483ecc6d9acd19fe873c3de6ee8a5d94d06f
bradonf333 <bradonf123@gmail.com>
2017-08-20 17:40:31 -0600

    Separated into feature modules

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ProductService } from './products/product.service';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent,
-    StarComponent, ProductDetailComponent,
-    ConvertToSpacesPipe
+    AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
-    , RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'product/:id', component: ProductDetailComponent },
+    BrowserModule, HttpClientModule,
+    RouterModule.forRoot([
       { path: '', redirectTo: 'products', pathMatch: 'full' },
       { path: '**', redirectTo: 'products', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProductListComponent } from './product-list.component';
+import { ProductService } from './product.service';
+import { StarComponent } from '../shared/star.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
+
 @NgModule({
-    declarations: [],
-    imports: [ CommonModule ],
+    declarations: [
+        ProductListComponent,
+        ProductDetailComponent,
+        ConvertToSpacesPipe,
+    ],
+    imports: [
+        SharedModule,
+        HttpClientModule,
+        RouterModule.forChild([
+            { path: 'products', component: ProductListComponent },
+            { path: 'product/:id', component: ProductDetailComponent }
+        ])
+    ],
     exports: [],
-    providers: [],
+    providers: [ ProductService ],
 })
 export class ProductModule {
 
